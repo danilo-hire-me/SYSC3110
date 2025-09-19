47acd9042a8aa87e0a8d1fe153cd49b42a2f1fe1
danbu <danilo.bukvic41@gmail.com>
2025-09-19 13:32:38 -0400

    Created main

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -16,10 +16,6 @@ public class AddressBook {
 
 
     public static void main(String[] args) {
-        BuddyInfo buddy = new BuddyInfo("Tom");
-        AddressBook addressBook = new AddressBook();
-        addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
         System.out.println("Adress Book");
     }
 }
